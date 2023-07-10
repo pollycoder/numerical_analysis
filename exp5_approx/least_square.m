2e5d27a7da55dbcc4120d3db49b817666c348556
Zhou Yi <94963357+pollycoder@users.noreply.github.com>
2023-07-10 12:57:03 +0800

    Update least_square.m

--- exp5_approx/least_square.m
+++ exp5_approx/least_square.m
@@ -1,13 +1,13 @@
-
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Least square method
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [coeffs] = least_square(x, y, n)
-    % 构建Vandermonde矩阵
+    % Gram matrix
     A = zeros(length(x), n+1);
     for i = 1:n+1
         A(:, i) = x.^(n+1-i);
     end
-    % 计算最小二乘解
+    
+    % result
     coeffs = A\y;
 end
